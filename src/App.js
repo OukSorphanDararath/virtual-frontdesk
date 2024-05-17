139fa9857334d9d0fd5181bae852aa51a5c66892
OukSorphanDararath <dararoth.ouk28@gmail.com>
2024-05-17 15:26:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Dashboard from "./Dashboard/Dashboard";
+// import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+      <p>HEllso</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  const today = new Date();
+
+  console.log(today, "KKK");
+
+  return <div>{/* <span>{today}</span> */}</div>;
+};
+
+export default Header;
