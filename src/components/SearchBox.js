58a6fd6d6ec5d217fe657a7622c4999460988017
OukSorphanDararath <dararoth.ouk28@gmail.com>
2024-06-05 22:18:43 +0700

    .

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 z-10 flex items-center ps-6 pointer-events-none">
+        <FiSearch className="w-6 h-6 text-gray-200" />
+      </div>
+      <input
+        type="search"
+        id="default-search"
+        className={`block w-96 focus:outline-none py-3 ps-16 text-lg shadow-inner rounded-full bg-white bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20`}
+        placeholder="Search"
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
