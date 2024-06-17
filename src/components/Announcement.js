20f4acfa63d0b86dc5b1884040693260988adabf
OukSorphanDararath <dararoth.ouk28@gmail.com>
2024-06-18 06:18:20 +0700

    .

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Announcement = () => {
   return (
-    <div className="border-2 border-white h-full basis-4/12 bg-white/50 rounded-2xl shadow-[rgba(0,_0,_0,_0.24)_0px_10px_8px]"></div>
-  )
-}
+    <div className="border-2 border-white h-full basis-4/12 bg-white/50 rounded-2xl shadow-[rgba(0,_0,_0,_0.24)_0px_10px_8px]">
+      Hello World
+    </div>
+  );
+};
 
-export default Announcement
+export default Announcement;
