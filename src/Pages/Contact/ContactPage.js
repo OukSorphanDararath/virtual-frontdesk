b3148e88a2fabaf1af073cb3e3d0f42943db0573
OukSorphanDararath <dararoth.ouk28@gmail.com>
2024-07-07 16:16:52 +0700

    .

--- src/Pages/Contact/ContactPage.js
+++ src/Pages/Contact/ContactPage.js
@@ -3,6 +3,7 @@ import Card from "../../components/Card";
 import pucLogo from "../../assets/puc-logo.png";
 import Button from "../../components/Button";
 import { MdQrCodeScanner } from "react-icons/md";
+import { IoCall } from "react-icons/io5";
 import Popup from "../../components/Popup";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
@@ -27,10 +28,27 @@ const ContactPage = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold my-8">Find PUC Contact</h1>
+      <div className="flex justify-between ">
+        <h1 className="text-3xl font-semibold my-8">Find PUC Contact</h1>
+        <Button
+          className={
+            "self-end bg-white mb-4 font-semibold hover:bg-gray-100 active:bg-gray-200"
+          }
+          text={
+            <div className="flex justify-center gap-2 items-center text-blue-950">
+              <IoCall />
+              <span>Talk to an agent</span>
+            </div>
+          }
+          onClick={() => history.push("/call")}
+        />
+      </div>
       <div className="absolute left-0 right-0 flex overflow-x-auto no-scrollbar pl-10 py-4 flex-nowrap">
         {contactData?.map((item) => (
-          <Card className="bg-white text-blue-900 relative mr-5" key={item?._id}>
+          <Card
+            className="bg-white text-blue-900 relative mr-5 "
+            key={item?._id}
+          >
             <div className="h-2/5 bg-gradient-to-t from-blue-200 px-6 py-4 relative">
               <img
                 src={pucLogo}
@@ -50,15 +68,17 @@ const ContactPage = () => {
               <div className="flex flex-col gap-3 relative">
                 <span className="font-normal">Telegram</span>
                 <span className="font-semibold text-2xl">{item?.telegram}</span>
-                <button onClick={() => setShowQRcode(item?._id)}>
-                  <MdQrCodeScanner
-                    size={50}
-                    className="absolute right-0 top-2"
-                  />
-                </button>
               </div>
               <div className="flex justify-center absolute bottom-6 left-0 right-0">
-                <Button text={"Call"} onClick={() => history.push("/call")} />
+                <Button
+                  text={
+                    <div className="flex justify-center gap-2 items-center">
+                      <MdQrCodeScanner size={24} />
+                      <span>Add Telegram</span>
+                    </div>
+                  }
+                  onClick={() => setShowQRcode(item?._id)}
+                />
               </div>
             </div>
             {showQRcode === item?._id && (
